ab26122579c8142569c28e8004d7b5c0dc9d7d8e
kaniffoll <stakot2902@gmail.com>
2026-09-06 14:35:04 +0300

    some changes in colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,25 @@
-return { 
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('kanagawa').setup({
-            transparent = true, 
-        })
-        vim.cmd("colorscheme kanagawa")
-    end,
+return {
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup({
+			transparent = true,
+			overrides = function(colors)
+				local theme = colors.theme
+				return {
+					LineNr = { bg = "none", fg = theme.ui.nontext },
+					CursorLineNr = { bg = "none", fg = theme.ui.special, bold = true },
+					SignColumn = { bg = "none" },
+					FoldColumn = { bg = "none", fg = theme.ui.nontext },
+
+					DiagnosticSignError = { bg = "none", fg = theme.diag.error },
+					DiagnosticSignWarn = { bg = "none", fg = theme.diag.warning },
+					DiagnosticSignInfo = { bg = "none", fg = theme.diag.info },
+					DiagnosticSignHint = { bg = "none", fg = theme.diag.hint },
+				}
+			end,
+		})
+		vim.cmd("colorscheme kanagawa")
+	end,
 }
